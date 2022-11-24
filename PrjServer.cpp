c744c98d9ef0f8afe52ae5fae43f52f8eaeceabd
성인 김 <ksi132@naver.com>
2022-11-25 04:35:46 +0900

    22.11.25
    UDP IPv6 송/수신 스레드 기능 추
    
    #define MULTICAST_RECV_IPv4 "235.7.8.1"  (서버가 가입 되어있는 멀티캐스트 주소)
    #define MULTICAST_SEND_TO_CLIENT_IPv4 "235.7.8.2" (클라이언트가 가입 되어있는 멀티캐스트 주소)
    
    #define MULTICAST_RECV_IPv6 "FF12::1:2:3:9"  (서버가 가입 되어있는 멀티캐스트 주소)
    #define MULTICAST_SEND_TO_CLIENT_IPv6 "FF12::1:2:3:4"(클라이언트가 가입 되어있는 멀티캐스트 주소)
    
    + DWORD WINAPI UDPv6_Multicast(LPVOID);
    
    # 추가해야할 기능들
    1- 콘솔 프로그램말고
    WINAPI WinMain 을 이용해서 창프로그램으로 GUI구현 후 클라이언트 관리할수잇어야함.
    
    2- main에 있는 TCP연결 구간을 스레드로 변경할 것
    
    3- TCP_IPv4, TCP_IPv6, UDP_IPv4, UDP_IPv6
    로 들어온 모든 클라이언트 연결들이 각각 서로에게 보내줄수있도록
    서로다른 프로토콜로 연결한 클라이언트들에게 어떻게 값을 뿌려줄지 고민해야함

--- PrjServer.cpp
+++ PrjServer.cpp
@@ -4,10 +4,17 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <stdio.h>
+#pragma warning(disable : 4996)
 
-#define MCAST "235.7.8.1"
-#define MULTICASTIPv4 "235.7.8.2"
-#define MULTICASTIPv6 "FF12::1:2:3:4"
+#define WM_SOCKET  (WM_USER+1)
+
+#define MULTICAST_RECV_IPv4 "235.7.8.1"
+#define MULTICAST_SEND_TO_CLIENT_IPv4 "235.7.8.2"
+
+
+#define MULTICAST_RECV_IPv6 "FF12::1:2:3:9"
+#define MULTICAST_SEND_TO_CLIENT_IPv6 "FF12::1:2:3:4"
+
 #define SERVERPORT 9000
 #define REMOTEPORT 9000
 #define BUFSIZE    256+1
@@ -22,7 +29,7 @@ struct SOCKETINFO
 };
 
 int nTotalSockets = 0;
-SOCKETINFO *SocketInfoArray[FD_SETSIZE];
+SOCKETINFO* SocketInfoArray[FD_SETSIZE];
 
 struct SOCKET_SendnRecv {
 	SOCKET recv;
@@ -37,23 +44,26 @@ void RemoveSocketInfo(int nIndex);
 DWORD WINAPI UDPv4_Multicast(LPVOID);
 DWORD WINAPI UDPv6_Multicast(LPVOID);
 
-// 오류 출력 함수
-void err_quit(char *msg);
-void err_display(char *msg);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int main(int argc, char *argv[])
+// 오류 출력 함수
+void err_quit(char* msg);
+void err_display(char* msg);
+
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
 {
 	int retval, retvalUDP;
 
 	// 윈속 초기화
 	WSADATA wsa;
-	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0) return 1;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
 
 	/*----- IPv4 소켓 초기화 시작 -----*/
 	// socket()
 	//printf("IPv4 TCP\n");
 	SOCKET listen_sockv4 = socket(AF_INET, SOCK_STREAM, 0);
-	if(listen_sockv4 == INVALID_SOCKET) err_quit("socket()");
+	if (listen_sockv4 == INVALID_SOCKET) err_quit("socket()");
 
 	// bind()
 	SOCKADDR_IN serveraddrv4;
@@ -61,19 +71,19 @@ int main(int argc, char *argv[])
 	serveraddrv4.sin_family = AF_INET;
 	serveraddrv4.sin_addr.s_addr = htonl(INADDR_ANY);
 	serveraddrv4.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sockv4, (SOCKADDR *)&serveraddrv4, sizeof(serveraddrv4));
-	if(retval == SOCKET_ERROR) err_quit("bind()");
+	retval = bind(listen_sockv4, (SOCKADDR*)&serveraddrv4, sizeof(serveraddrv4));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
 
 	// listen()
 	retval = listen(listen_sockv4, SOMAXCONN);
-	if(retval == SOCKET_ERROR) err_quit("listen()");
+	if (retval == SOCKET_ERROR) err_quit("listen()");
 	/*----- IPv4 소켓 초기화 끝 -----*/
 
 	/*----- IPv6 소켓 초기화 시작 -----*/
 	// socket()
 	//printf("IPv6 TCP\n");
 	SOCKET listen_sockv6 = socket(AF_INET6, SOCK_STREAM, 0);
-	if(listen_sockv6 == INVALID_SOCKET) err_quit("socket()");
+	if (listen_sockv6 == INVALID_SOCKET) err_quit("socket()");
 
 	// bind()
 	SOCKADDR_IN6 serveraddrv6;
@@ -81,12 +91,12 @@ int main(int argc, char *argv[])
 	serveraddrv6.sin6_family = AF_INET6;
 	serveraddrv6.sin6_addr = in6addr_any;
 	serveraddrv6.sin6_port = htons(SERVERPORT);
-	retval = bind(listen_sockv6, (SOCKADDR *)&serveraddrv6, sizeof(serveraddrv6));
-	if(retval == SOCKET_ERROR) err_quit("bind()");
+	retval = bind(listen_sockv6, (SOCKADDR*)&serveraddrv6, sizeof(serveraddrv6));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
 
 	// listen()
 	retval = listen(listen_sockv6, SOMAXCONN);
-	if(retval == SOCKET_ERROR) err_quit("listen()");
+	if (retval == SOCKET_ERROR) err_quit("listen()");
 	/*----- IPv6 소켓 초기화 끝 -----*/
 
 
@@ -99,7 +109,7 @@ int main(int argc, char *argv[])
 	SOCKET send_sock_UDPv4 = socket(AF_INET, SOCK_DGRAM, 0);
 	if (send_sock_UDPv4 == INVALID_SOCKET) err_quit("socket()");
 
-	struct SOCKET_SendnRecv UDPv4 = {listen_sock_UDPv4, send_sock_UDPv4};
+	struct SOCKET_SendnRecv UDPv4 = { listen_sock_UDPv4, send_sock_UDPv4 };
 	SOCKET_SendnRecv* UDPv4_P = &UDPv4;
 
 	/*----- UDP IPv4 소켓 초기화 끝 -----*/
@@ -123,9 +133,8 @@ int main(int argc, char *argv[])
 
 	HANDLE hThread[3];
 	hThread[0] = CreateThread(NULL, 0, UDPv4_Multicast, (LPVOID)UDPv4_P, 0, NULL);
-
-	//DWORD please = WaitForMultipleObjects(1, hThread, TRUE, INFINITE);
-	DWORD please = WaitForSingleObject(hThread[0], INFINITE);
+	hThread[1] = CreateThread(NULL, 0, UDPv6_Multicast, (LPVOID)UDPv6_P, 0, NULL);
+	DWORD please = WaitForMultipleObjects(2, hThread, TRUE, INFINITE);
 
 
 	/* -------- 스레드화 해야함! --------- */
@@ -138,8 +147,8 @@ int main(int argc, char *argv[])
 	// 데이터 통신에 사용할 변수(IPv6)
 	SOCKADDR_IN6 clientaddrv6;
 
-	
-	while(1){ 
+
+	while (1) {
 		// 소켓 셋 초기화
 		FD_ZERO(&rset);
 		FD_SET(listen_sockv4, &rset);
@@ -149,26 +158,26 @@ int main(int argc, char *argv[])
 		FD_SET(listen_sock_UDPv4, &rset);
 		FD_SET(listen_sock_UDPv6, &rset);
 
-		for(i=0; i<nTotalSockets; i++){
+		for (i = 0; i < nTotalSockets; i++) {
 			FD_SET(SocketInfoArray[i]->sock, &rset);
 		}
 
 		// select()
 		retval = select(0, &rset, NULL, NULL, NULL);
-		if(retval == SOCKET_ERROR){
+		if (retval == SOCKET_ERROR) {
 			err_display("select()");
 			break;
 		}
 
 		// 소켓 셋 검사(1): 클라이언트 접속 수용
-		if(FD_ISSET(listen_sockv4, &rset)){
+		if (FD_ISSET(listen_sockv4, &rset)) {
 			addrlen = sizeof(clientaddrv4);
-			client_sock = accept(listen_sockv4, (SOCKADDR *)&clientaddrv4, &addrlen);
-			if(client_sock == INVALID_SOCKET){
+			client_sock = accept(listen_sockv4, (SOCKADDR*)&clientaddrv4, &addrlen);
+			if (client_sock == INVALID_SOCKET) {
 				err_display("accept()");
 				break;
 			}
-			else{
+			else {
 				// 접속한 클라이언트 정보 출력
 				printf("[TCPv4 서버] 클라이언트 접속: [%s]:%d\n",
 					inet_ntoa(clientaddrv4.sin_addr), ntohs(clientaddrv4.sin_port));
@@ -176,18 +185,18 @@ int main(int argc, char *argv[])
 				AddSocketInfo(client_sock, false);
 			}
 		}
-		if(FD_ISSET(listen_sockv6, &rset)){
+		if (FD_ISSET(listen_sockv6, &rset)) {
 			addrlen = sizeof(clientaddrv6);
-			client_sock = accept(listen_sockv6, (SOCKADDR *)&clientaddrv6, &addrlen);
-			if(client_sock == INVALID_SOCKET){
+			client_sock = accept(listen_sockv6, (SOCKADDR*)&clientaddrv6, &addrlen);
+			if (client_sock == INVALID_SOCKET) {
 				err_display("accept()");
 				break;
 			}
-			else{
+			else {
 				// 접속한 클라이언트 정보 출력
 				char ipaddr[50];
 				DWORD ipaddrlen = sizeof(ipaddr);
-				WSAAddressToString((SOCKADDR *)&clientaddrv6, sizeof(clientaddrv6),
+				WSAAddressToString((SOCKADDR*)&clientaddrv6, sizeof(clientaddrv6),
 					NULL, ipaddr, &ipaddrlen);
 				printf("[TCPv6 서버] 클라이언트 접속: %s\n", ipaddr);
 				// 소켓 정보 추가
@@ -196,28 +205,28 @@ int main(int argc, char *argv[])
 		}
 
 		// 소켓 셋 검사(2): 데이터 통신
-		for(i=0; i<nTotalSockets; i++){
-			SOCKETINFO *ptr = SocketInfoArray[i];
-			if(FD_ISSET(ptr->sock, &rset)){
+		for (i = 0; i < nTotalSockets; i++) {
+			SOCKETINFO* ptr = SocketInfoArray[i];
+			if (FD_ISSET(ptr->sock, &rset)) {
 				// 데이터 받기
 				retval = recv(ptr->sock, ptr->buf + ptr->recvbytes,
 					BUFSIZE - ptr->recvbytes, 0);
-				if(retval == 0 || retval == SOCKET_ERROR){
+				if (retval == 0 || retval == SOCKET_ERROR) {
 					RemoveSocketInfo(i);
 					continue;
 				}
 
 				// 받은 바이트 수 누적
 				ptr->recvbytes += retval;
-				if(ptr->recvbytes == BUFSIZE){
+				if (ptr->recvbytes == BUFSIZE) {
 					// 받은 바이트 수 리셋
 					ptr->recvbytes = 0;
 
 					// 현재 접속한 모든 클라이언트에게 데이터를 보냄!
-					for(j=0; j<nTotalSockets; j++){
-						SOCKETINFO *ptr2 = SocketInfoArray[j];
+					for (j = 0; j < nTotalSockets; j++) {
+						SOCKETINFO* ptr2 = SocketInfoArray[j];
 						retval = send(ptr2->sock, ptr->buf, BUFSIZE, 0);
-						if(retval == SOCKET_ERROR){
+						if (retval == SOCKET_ERROR) {
 							err_display("send()");
 							RemoveSocketInfo(j);
 							--j; // 루프 인덱스 보정
@@ -231,6 +240,8 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
+
 
 DWORD WINAPI UDPv4_Multicast(LPVOID arg) {
 	SOCKET_SendnRecv* socks = (SOCKET_SendnRecv*)arg;
@@ -260,7 +271,7 @@ DWORD WINAPI UDPv4_Multicast(LPVOID arg) {
 
 	// 멀티캐스트 그룹 가입
 	struct ip_mreq mreq_v4;
-	mreq_v4.imr_multiaddr.s_addr = inet_addr(MCAST);
+	mreq_v4.imr_multiaddr.s_addr = inet_addr(MULTICAST_RECV_IPv4);
 	mreq_v4.imr_interface.s_addr = htonl(INADDR_ANY);
 	retvalUDP = setsockopt(listen_sock_UDPv4, IPPROTO_IP, IP_ADD_MEMBERSHIP,
 		(char*)&mreq_v4, sizeof(mreq_v4));
@@ -281,7 +292,7 @@ DWORD WINAPI UDPv4_Multicast(LPVOID arg) {
 	SOCKADDR_IN remoteaddr_v4;
 	ZeroMemory(&remoteaddr_v4, sizeof(remoteaddr_v4));
 	remoteaddr_v4.sin_family = AF_INET;
-	remoteaddr_v4.sin_addr.s_addr = inet_addr(MULTICASTIPv4);
+	remoteaddr_v4.sin_addr.s_addr = inet_addr(MULTICAST_SEND_TO_CLIENT_IPv4);
 	remoteaddr_v4.sin_port = htons(REMOTEPORT);
 	char ipaddr[50];
 	DWORD ipaddrlen = sizeof(ipaddr);
@@ -289,7 +300,7 @@ DWORD WINAPI UDPv4_Multicast(LPVOID arg) {
 	while (1) {
 		ZeroMemory(buf_UDP, BUFSIZE);
 		addrlen_UDP = sizeof(peeraddr_v4);
-		
+
 		retvalUDP = recvfrom(listen_sock_UDPv4, buf_UDP, BUFSIZE, 0,
 			(SOCKADDR*)&peeraddr_v4, &addrlen_UDP); // peeraddr NULL 로 해도 ㄱc 
 
@@ -302,9 +313,9 @@ DWORD WINAPI UDPv4_Multicast(LPVOID arg) {
 
 		WSAAddressToString((SOCKADDR*)&peeraddr_v4, sizeof(peeraddr_v4), NULL, ipaddr, &ipaddrlen);
 		printf("[UDPv4 서버] 클라이언트 데이터 수신: %s\n", ipaddr); // 서버가 보낸게 아니라면, 서버는 맨끝 바이트를 -1로 초기화
-		if (buf_UDP[BUFSIZE-1] == -1) continue;
+		if (buf_UDP[BUFSIZE - 1] == -1) continue;
 
-		buf_UDP[BUFSIZE-1] = -1;
+		buf_UDP[BUFSIZE - 1] = -1;
 		retvalUDP = sendto(send_sock_UDPv4, buf_UDP, BUFSIZE, 0,
 			(SOCKADDR*)&remoteaddr_v4, sizeof(remoteaddr_v4));
 		if (retvalUDP == SOCKET_ERROR) {
@@ -316,12 +327,14 @@ DWORD WINAPI UDPv4_Multicast(LPVOID arg) {
 
 DWORD WINAPI UDPv6_Multicast(LPVOID arg) {
 	SOCKET_SendnRecv* socks = (SOCKET_SendnRecv*)arg;
-	printf("UDPv6_Multicast : %p\n", socks);
 	SOCKET listen_sock_UDPv6 = socks->recv;
 	SOCKET send_sock_UDPv6 = socks->send;
 	SOCKADDR_IN6 peeraddr_v6;
 	int addrlen_UDP;
 	char buf_UDP[BUFSIZE];
+
+	char ipaddr[50];
+	DWORD ipaddrlen = sizeof(ipaddr);
 
 	// <receiving>
 	// SO_REUSEADDR 소켓 옵션 설정
@@ -329,7 +342,6 @@ DWORD WINAPI UDPv6_Multicast(LPVOID arg) {
 	int retvalUDP = setsockopt(listen_sock_UDPv6, SOL_SOCKET,
 		SO_REUSEADDR, (char*)&optval, sizeof(optval));
 	if (retvalUDP == SOCKET_ERROR) {
-		printf("sockopt()3\n");
 		err_quit("setsockopt()");
 	}
 
@@ -344,7 +356,7 @@ DWORD WINAPI UDPv6_Multicast(LPVOID arg) {
 	// 주소 변환(문자열 -> IPv6)
 	SOCKADDR_IN6 tmpaddr;
 	int templen = sizeof(tmpaddr);
-	WSAStringToAddress(MULTICASTIPv6, AF_INET6, NULL,
+	WSAStringToAddress(MULTICAST_RECV_IPv6, AF_INET6, NULL,
 		(SOCKADDR*)&tmpaddr, &templen);
 
 	// 멀티캐스트 그룹 가입
@@ -354,7 +366,6 @@ DWORD WINAPI UDPv6_Multicast(LPVOID arg) {
 	retvalUDP = setsockopt(listen_sock_UDPv6, IPPROTO_IPV6, IPV6_ADD_MEMBERSHIP,
 		(char*)&mreq_v6, sizeof(mreq_v6));
 	if (retvalUDP == SOCKET_ERROR) {
-		printf("sockopt()4\n");
 		err_quit("setsockopt()");
 	}
 
@@ -373,19 +384,25 @@ DWORD WINAPI UDPv6_Multicast(LPVOID arg) {
 	ZeroMemory(&remoteaddr_v6, sizeof(remoteaddr_v6));
 	remoteaddr_v6.sin6_family = AF_INET6;
 	int remoteaddr6_len = sizeof(remoteaddr_v6);
-	WSAStringToAddress(MULTICASTIPv6, AF_INET6, NULL,
+	WSAStringToAddress(MULTICAST_SEND_TO_CLIENT_IPv6, AF_INET6, NULL,
 		(SOCKADDR*)&remoteaddr_v6, &remoteaddr6_len);
 	remoteaddr_v6.sin6_port = htons(REMOTEPORT);
-	
+
 	while (1) {
 		addrlen_UDP = sizeof(peeraddr_v6);
 		retvalUDP = recvfrom(listen_sock_UDPv6, buf_UDP, BUFSIZE, 0,
-			(SOCKADDR*)&peeraddr_v6, &addrlen_UDP); // peeraddr NULL 로 해도 ㄱㅊ
+			(SOCKADDR*)&peeraddr_v6, &addrlen_UDP);
 		if (retvalUDP == SOCKET_ERROR) {
 			err_display("recvfrom()");
+			continue;
 		}
 
-		retvalUDP = sendto(listen_sock_UDPv6, buf_UDP, strlen(buf_UDP), 0,
+		WSAAddressToString((SOCKADDR*)&peeraddr_v6, sizeof(peeraddr_v6), NULL, ipaddr, &ipaddrlen);
+		printf("[UDPv6 서버] 클라이언트 데이터 수신: %s\n", ipaddr); // 서버가 보낸게 아니라면, 서버는 맨끝 바이트를 -1로 초기화
+		if (buf_UDP[BUFSIZE - 1] == -1) continue;
+
+		buf_UDP[BUFSIZE - 1] = -1;
+		retvalUDP = sendto(send_sock_UDPv6, buf_UDP, BUFSIZE, 0,
 			(SOCKADDR*)&remoteaddr_v6, sizeof(remoteaddr_v6));
 		if (retvalUDP == SOCKET_ERROR) {
 			err_display("sendto()");
@@ -397,13 +414,13 @@ DWORD WINAPI UDPv6_Multicast(LPVOID arg) {
 // 소켓 정보 추가
 BOOL AddSocketInfo(SOCKET sock, bool isIPv6)
 {
-	if(nTotalSockets >= FD_SETSIZE){
+	if (nTotalSockets >= FD_SETSIZE) {
 		printf("[오류] 소켓 정보를 추가할 수 없습니다!\n");
 		return FALSE;
 	}
 
-	SOCKETINFO *ptr = new SOCKETINFO;
-	if(ptr == NULL){
+	SOCKETINFO* ptr = new SOCKETINFO;
+	if (ptr == NULL) {
 		printf("[오류] 메모리가 부족합니다!\n");
 		return FALSE;
 	}
@@ -419,24 +436,24 @@ BOOL AddSocketInfo(SOCKET sock, bool isIPv6)
 // 소켓 정보 삭제
 void RemoveSocketInfo(int nIndex)
 {
-	SOCKETINFO *ptr = SocketInfoArray[nIndex];
+	SOCKETINFO* ptr = SocketInfoArray[nIndex];
 
 	// 종료한 클라이언트 정보 출력
-	if(ptr->isIPv6 == false){
+	if (ptr->isIPv6 == false) {
 		SOCKADDR_IN clientaddrv4;
 		int addrlen = sizeof(clientaddrv4);
-		getpeername(ptr->sock, (SOCKADDR *)&clientaddrv4, &addrlen);
-		printf("[TCPv4 서버] 클라이언트 종료: [%s]:%d\n", 
+		getpeername(ptr->sock, (SOCKADDR*)&clientaddrv4, &addrlen);
+		printf("[TCPv4 서버] 클라이언트 종료: [%s]:%d\n",
 			inet_ntoa(clientaddrv4.sin_addr), ntohs(clientaddrv4.sin_port));
 	}
-	else{
+	else {
 		SOCKADDR_IN6 clientaddrv6;
 		int addrlen = sizeof(clientaddrv6);
-		getpeername(ptr->sock, (SOCKADDR *)&clientaddrv6, &addrlen);
+		getpeername(ptr->sock, (SOCKADDR*)&clientaddrv6, &addrlen);
 
 		char ipaddr[50];
 		DWORD ipaddrlen = sizeof(ipaddr);
-		WSAAddressToString((SOCKADDR *)&clientaddrv6, sizeof(clientaddrv6),
+		WSAAddressToString((SOCKADDR*)&clientaddrv6, sizeof(clientaddrv6),
 			NULL, ipaddr, &ipaddrlen);
 		printf("[TCPv6 서버] 클라이언트 종료: %s\n", ipaddr);
 	}
@@ -444,18 +461,18 @@ void RemoveSocketInfo(int nIndex)
 	closesocket(ptr->sock);
 	delete ptr;
 
-	if(nIndex != (nTotalSockets-1))
-		SocketInfoArray[nIndex] = SocketInfoArray[nTotalSockets-1];
+	if (nIndex != (nTotalSockets - 1))
+		SocketInfoArray[nIndex] = SocketInfoArray[nTotalSockets - 1];
 
 	--nTotalSockets;
 }
 
 // 소켓 함수 오류 출력 후 종료
-void err_quit(char *msg)
+void err_quit(char* msg)
 {
 	LPVOID lpMsgBuf;
 	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
 		NULL, WSAGetLastError(),
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPTSTR)&lpMsgBuf, 0, NULL);
@@ -465,14 +482,14 @@ void err_quit(char *msg)
 }
 
 // 소켓 함수 오류 출력
-void err_display(char *msg)
+void err_display(char* msg)
 {
 	LPVOID lpMsgBuf;
 	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
 		NULL, WSAGetLastError(),
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPTSTR)&lpMsgBuf, 0, NULL);
-	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	printf("[%s] %s", msg, (char*)lpMsgBuf);
 	LocalFree(lpMsgBuf);
 }
